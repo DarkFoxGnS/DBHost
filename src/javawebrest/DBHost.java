3b383c699304d5d3066be27eb6c4f0423f955957
DarkFox_GnS <szabotibor0123456789@gmail.com>
2023-08-22 18:30:11 +0200

    Added readme

--- src/javawebrest/DBHost.java
+++ src/javawebrest/DBHost.java
@@ -30,6 +30,7 @@ public class DBHost {
         try {
             ServerSocket ss = new ServerSocket(80);//create new server on HTTP (80) port
             System.out.println("Done");
+            System.out.println("Waiting for connections...");
             while (true) {
                 Socket client = ss.accept(); // wait till a connection is requested
                 System.out.println("\u001B[41m" + "Connection from: " + client.getInetAddress()); //display ip of connected device
